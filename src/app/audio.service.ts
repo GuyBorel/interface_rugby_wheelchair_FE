eaba7de4fe25d700b6ed9b3064ef8a4b110801e5
Belgnaoui Othman <belgnaoui.othman@gmail.com>
2024-05-01 22:56:59 +0200

    Modification de l'url de l'api audio.service.ts

--- src/app/audio.service.ts
+++ src/app/audio.service.ts
@@ -11,20 +11,21 @@ interface TranscriptionResponse {
   providedIn: 'root'
 })
 export class AudioService {
+  private apiUrl:string = 'http://obvault.duckdns.org'
   constructor(private http: HttpClient) {
   }
 
   uploadAudio(file: File) {
     const formData = new FormData();
     formData.append('audio', file, file.name);
-    return this.http.post('http://127.0.0.1:5000/upload', formData);
+    return this.http.post(this.apiUrl+'/upload', formData);
   }
 
   uploadAudioForTranscription(file: File): Observable<TranscriptionResponse> {
     const formData = new FormData();
     formData.append('file', file);
 
-    return this.http.post<TranscriptionResponse>('http://127.0.0.1:5000/transcribe', formData).pipe(
+    return this.http.post<TranscriptionResponse>(this.apiUrl+'/transcribe', formData).pipe(
       tap(response => console.log("Réponse de transcription:", response)),
       catchError((error: any) => { // Explicitement typé 'error' comme 'any' ou utilisez un type plus spécifique si possible
         console.error("Erreur lors de la transcription:", error);
