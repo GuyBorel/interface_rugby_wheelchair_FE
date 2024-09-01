870e2ba478021e81e99f84abfb64627f97c79287
guy-borel.tabueu-kamga <guy-borel.tabueu-kamga@adesso.de>
2024-09-01 21:09:25 +0200

    update current game version from 0.0.0 to 0.1.0

--- src/app/modules/games/games.component.ts
+++ src/app/modules/games/games.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GameService, Match} from "../../services/game.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './games.component.html',
   styleUrl: './games.component.css',
 })
-export class GamesComponent {}
+export class GamesComponent implements OnInit {
+  matchs: Match[] = [];
+
+  constructor(private matchService: GameService) { }
+
+  ngOnInit(): void {
+    this.matchs = this.matchService.getMatchs();
+  }
+}
