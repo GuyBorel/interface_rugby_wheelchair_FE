9c201db29f2377fa0c38a5ac3205cbae49bf8071
Bebel19 <belgnaoui.othman@gmail.com>
2024-09-01 21:59:40 +0200

    Added match table model

---
+++ src/app/models/match.model.ts
@@ -0,0 +1,8 @@
+export interface Match {
+  id: number;
+  date: string;
+  team_1: string;         // The name or ID of the first team
+  team_2: string;         // The name or ID of the second team
+  team_1_score: number;
+  team_2_score: number;
+}
