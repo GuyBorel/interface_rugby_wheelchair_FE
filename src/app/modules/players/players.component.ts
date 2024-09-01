11ff3ebf8e27910c039c2a59d33440974222ade9
Bebel19 <belgnaoui.othman@gmail.com>
2024-09-01 21:05:15 +0200

    Added player's picture in the players list

--- src/app/modules/players/players.component.ts
+++ src/app/modules/players/players.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { FormsModule } from '@angular/forms';  // Add this for ngModel support
 import { DatabaseService } from '../../services/database.service'; // Ensure the service path is correct
 
 @Component({
   selector: 'app-players',
   standalone: true,
-  imports: [CommonModule, FormsModule], // Add CommonModule and FormsModule
+  imports: [CommonModule, FormsModule, NgOptimizedImage], // Add CommonModule and FormsModule
   templateUrl: './players.component.html',
 })
 export class PlayersComponent implements OnInit {
   teams: any[] = [];
   players: any[] = [];
+  attackers: any[] = []; // Declare the attackers array
+  defenders: any[] = []; // Declare the defenders array
   selectedTeamId: number | null = null;
 
   constructor(private databaseService: DatabaseService) {}
@@ -32,6 +34,10 @@ export class PlayersComponent implements OnInit {
       this.databaseService.getPlayers(this.selectedTeamId).subscribe(
         (data) => {
           this.players = data;
+          this.attackers = this.players.filter(player => player.position === 'Attacker');
+          this.defenders = this.players.filter(player => player.position === 'Defender');
+          console.log('Attackers:', this.attackers);
+          console.log('Defenders:', this.defenders);
         },
         (error) => {
           console.error('Error fetching players:', error);
@@ -39,4 +45,16 @@ export class PlayersComponent implements OnInit {
       );
     }
   }
+
+
+  imageError(event: any) {
+    event.target.src = 'assets/images/default-placeholder.png';
+  }
+
+  categorizePlayers() {
+    this.attackers = this.players.filter(player => player.position === 'Attacker');
+    this.defenders = this.players.filter(player => player.position === 'Defender');
+  }
+
+  protected readonly Math = Math;
 }
