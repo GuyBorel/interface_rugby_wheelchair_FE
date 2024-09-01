65e3e1c403f02f30afa544132f2b87414e820fea
guy-borel.tabueu-kamga <guy-borel.tabueu-kamga@adesso.de>
2024-09-01 19:30:54 +0200

    update current game version from 0.0.0 to 0.1.0

--- src/app/modules/players/players.component.ts
+++ src/app/modules/players/players.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-players',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './players.component.html',
   styleUrl: './players.component.css',
 })
-export class PlayersComponent {}
+export class PlayersComponent {
+  items = [
+    { column1: 'Row 1, Column 1', column2: 'Row 1, Column 2' },
+    { column1: 'Row 2, Column 1', column2: 'Row 2, Column 2' },
+    { column1: 'Row 3, Column 1', column2: 'Row 3, Column 2' },
+    // Add more rows as needed
+  ];
+}
