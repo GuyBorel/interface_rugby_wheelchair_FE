ff627f1ce897c5afa839e3b5ffa7b962d872f847
Bebel19 <belgnaoui.othman@gmail.com>
2024-09-02 22:39:11 +0200

    Added games history

--- src/app/modules/games/games.component.ts
+++ src/app/modules/games/games.component.ts
@@ -1,22 +1,47 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from '../../services/database.service';
-import {NgForOf} from "@angular/common";
+import { Match } from '../../models/match.model';
+import {DatePipe, NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [
-    NgForOf
-  ],
   templateUrl: './games.component.html',
-  styleUrl: './games.component.css',
+  imports: [
+    DatePipe,
+    FormsModule,
+    NgForOf
+  ]
 })
 export class GamesComponent implements OnInit {
-  matchs: Match[] = [];
+  championships: any[] = [];
+  selectedChampionshipId: number | null = null;
+  matches: Match[] = [];
 
-  constructor(private matchService: GameService) { }
+  constructor(private databaseService: DatabaseService) { }
 
   ngOnInit(): void {
-    this.matchs = this.matchService.getMatchs();
+    this.databaseService.getChampionships().subscribe(
+      (data) => {
+        this.championships = data;
+      },
+      (error) => {
+        console.error('Error fetching championships:', error);
+      }
+    );
+  }
+
+  onChampionshipSelect(): void {
+    if (this.selectedChampionshipId !== null) {
+      this.databaseService.getMatches(this.selectedChampionshipId).subscribe(
+        (data) => {
+          this.matches = data;
+        },
+        (error) => {
+          console.error('Error fetching matches:', error);
+        }
+      );
+    }
   }
 }
